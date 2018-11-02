776015e3678e4b36247eab1205152c19bdf71796
Ricardo Ledesma <rickydem24@gmail.com>
2018-11-02 16:17:49 -0600

    Adds test for generating key shifts

--- test/encoder_test.rb
+++ test/encoder_test.rb
@@ -5,7 +5,17 @@ class EncoderTest < Minitest::Test
     @encoder = Encoder.new
   end
 
+  def test_it_generates_key_shifts_given_random_five_digits
+    random = ["1", "2", "6", "0", "5"]
+    @encoder.stubs(:get_random_five_digit_number).returns(random)
+
+    expected = {A: 1, B: 2, C: 6, D: 0, E: 5}
+
+    assert_equal expected, @encoder.generate_key_shifts
+  end
+
   def test_it_generates_a_random_five_digit_number
+    skip
     result = @encoder.get_random_five_digit_number
 
     assert_instance_of Array, result
