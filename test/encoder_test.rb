7158519ea826556734556078a3df2cbfe857b81f
Ricardo Ledesma <rickydem24@gmail.com>
2018-11-02 16:50:04 -0600

    Adds expect to key shift test

--- test/encoder_test.rb
+++ test/encoder_test.rb
@@ -6,17 +6,17 @@ class EncoderTest < Minitest::Test
   end
 
   def test_it_generates_key_shifts_given_random_five_digits
+    skip
     random = ["1", "2", "6", "0", "5"]
     @encoder.stubs(:get_random_five_digit_number).returns(random)
 
-    expected = {A: 1, B: 2, C: 6, D: 0, E: 5}
+    expected = {A: 12, B: 26, C: 60, D: 5}
 
     assert_equal expected, @encoder.generate_key_shifts
   end
 
-  def test_it_generates_a_random_five_digit_number
-    skip
-    result = @encoder.get_random_five_digit_number
+  def test_it_generates_a_random_five_digit_number_array
+    result = @encoder.get_random_five_digit_number_array
 
     assert_instance_of Array, result
     assert_equal 5, result.length
