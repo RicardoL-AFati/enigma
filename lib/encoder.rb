d8ad6077e78b7f342ee942eb4fc8461281771631
Ricardo Ledesma <rickydem24@gmail.com>
2018-11-03 13:43:58 -0600

    Adds change in date format - DDMMYY

--- lib/encoder.rb
+++ lib/encoder.rb
@@ -30,11 +30,11 @@ class Encoder
   end
 
   def get_offset_code(date)
-    date = in_MM_DD_YY(date)
+    date = in_DD_MM_YY(date)
     (date.to_i ** 2).to_s[-4..-1].chars
   end
 
-  def in_MM_DD_YY(date)
-    date.strftime("%m%d%y")
+  def in_DD_MM_YY(date)
+    date.strftime("%d%m%y")
   end
 end
