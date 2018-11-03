44e6538a42dd27ffc94f1e105f9d429431fa2671
Ricardo Ledesma <rickydem24@gmail.com>
2018-11-03 14:08:57 -0600

    Moves methods and adds empty generate shifts

--- lib/encoder.rb
+++ lib/encoder.rb
@@ -1,11 +1,8 @@
 class Encoder
-  def get_random_five_digit_number_array
-    random = rand(9999999999999).to_s.chars.shuffle.slice(0, 5)
+  def generate_shifts
 
-    (5 - random.length).times { random.unshift("0") } if random.length < 5
-    random
   end
-
+  
   def generate_key_shifts
     random_key = get_random_five_digit_number_array
     random_key.each_with_index.reduce(empty_shifts) do |shifts, (digit, index)|
@@ -25,8 +22,11 @@ class Encoder
     end
   end
 
-  def empty_shifts
-    {A: "", B: "", C: "", D: ""}
+  def get_random_five_digit_number_array
+    random = rand(9999999999999).to_s.chars.shuffle.slice(0, 5)
+
+    (5 - random.length).times { random.unshift("0") } if random.length < 5
+    random
   end
 
   def get_offset_code(date)
@@ -36,5 +36,9 @@ class Encoder
 
   def in_DD_MM_YY(date)
     date.strftime("%d%m%y")
+  end
+
+  def empty_shifts
+    {A: "", B: "", C: "", D: ""}
   end
 end
