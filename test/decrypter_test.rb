6abd8231a6e0b81c43f33141ff9cd11270b100e5
Ricardo Ledesma <rickydem24@gmail.com>
2018-11-03 13:08:23 -0600

    adds change to shift test - no more default param

--- test/decrypter_test.rb
+++ test/decrypter_test.rb
@@ -5,7 +5,7 @@ class DecrypterTest < Minitest::Test
     @decrypter = Decrypter.new
   end
 
-  def test_it_encrypts_message
+  def test_it_decrypts_message
     expected = "hello world"
     result = @decrypter.decrypt("keder ohulw",
       [3, 27, 73, 20])
@@ -13,9 +13,9 @@ class DecrypterTest < Minitest::Test
     assert_equal expected, result
   end
 
-  def test_it_shifts_an_array_of_characters
-    expected = "kede"
-    result = @decrypter.shift(["h", "e", "l", "l"],
+  def test_it_shifts_an_array_of_characters_backwards
+    expected = "hell"
+    result = @decrypter.shift(["k", "e", "d", "e"],
       [3, 27, 73, 20], true)
 
     assert_equal expected, result
