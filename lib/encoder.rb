cb4f0bd282eae69515c31525fe7db05bd84a287c
Ricardo Ledesma <rickydem24@gmail.com>
2018-11-03 09:09:54 -0600

    Adds get_offset_code

--- lib/encoder.rb
+++ lib/encoder.rb
@@ -9,24 +9,28 @@ class Encoder
   def generate_key_shifts
     random_key = get_random_five_digit_number_array
     random_key.each_with_index.reduce(empty_shifts) do |shifts, (digit, index)|
-      key_shifts[:A] += digit if index == 0 || index == 1
-      key_shifts[:B] += digit if index == 1 || index == 2
-      key_shifts[:C] += digit if index == 2 || index == 3
-      key_shifts[:D] += digit if index == 3 || index == 4
-      key_shifts
+      shifts[:A] += digit if index == 0 || index == 1
+      shifts[:B] += digit if index == 1 || index == 2
+      shifts[:C] += digit if index == 2 || index == 3
+      shifts[:D] += digit if index == 3 || index == 4
+      shifts
     end.transform_values!(&:to_i)
   end
 
   def generate_offset_shifts(date = Time.now)
-    require "pry"; binding.pry
-    date_in_MM_DD_YY(date)
-    hash = mm_dd_yy.chars.each_with_index.reduce(empty_shifts) do |shifts, (num, index)|
-      shifts[empty_shifts.keys[index]] = num.to_i
+    code = get_offset_code(date)
+    code.each_with_index.reduce(empty_shifts) do |shifts, (num, index)|
+      shifts[shifts.keys[index]] = num.to_i
+      shifts
     end
-    require "pry"; binding.pry
   end
 
   def empty_shifts
     {A: "", B: "", C: "", D: ""}
+  end
+
+  def get_offset_code(date)
+    date = date_in_MM_DD_YY(date)
+    (date.to_i ** 2).to_s[-4..-1].chars
   end
 end
