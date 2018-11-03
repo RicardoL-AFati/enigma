c46e18c94f8c76eda54ff688f26d47fab8a6954e
Ricardo Ledesma <rickydem24@gmail.com>
2018-11-03 08:33:44 -0600

    Adds empty_shifts helper method

--- lib/encoder.rb
+++ lib/encoder.rb
@@ -8,13 +8,20 @@ class Encoder
 
   def generate_key_shifts
     random_key = get_random_five_digit_number_array
-    key_shifts = {A: "", B: "", C: "", D: ""}
-    random_key.each_with_index.reduce(key_shifts) do |key_shifts, (digit, index)|
+    random_key.each_with_index.reduce(empty_shifts) do |key_shifts, (digit, index)|
       key_shifts[:A] += digit if index == 0 || index == 1
       key_shifts[:B] += digit if index == 1 || index == 2
       key_shifts[:C] += digit if index == 2 || index == 3
       key_shifts[:D] += digit if index == 3 || index == 4
       key_shifts
     end.transform_values!(&:to_i)
+  end
+
+  def generate_offset_shifts(date = Time.now)
+    date_in_MM_DD_YY(date).chars.reduce({}) do
+  end
+
+  def empty_shifts
+    {A: "", B: "", C: "", D: ""}
   end
 end
