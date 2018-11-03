9617df20083d0610936bcc73fff6063d64f37840
Ricardo Ledesma <rickydem24@gmail.com>
2018-11-03 10:20:43 -0600

    Adds intial encrypt method

--- lib/encrypter.rb
+++ lib/encrypter.rb
@@ -2,5 +2,13 @@ class Encrypter
   ABC_ = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
           "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
 
-  
+  def encrypt(message, shifts)
+    message_chars = message.chars
+    encrypted = []
+
+    until message_chars.empty?
+      encrypted << shift(message_chars.slice!(0, 4))
+    end
+    encrypted
+  end
 end
