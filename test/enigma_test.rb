2d46fc55f082c0036d8ab81f3ff166a51adea411
Ricardo Ledesma <rickydem24@gmail.com>
2018-11-03 13:57:44 -0600

    Adds setup for tests, instance_vars and two encrypt tests

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -2,7 +2,38 @@ require './test/test_helper'
 
 class EnigmaTest < Minitest::Test
   def setup
-    @enigma = Enigma.new
+    @enigma = Enigma.new()
   end
 
+  def test_it_has_an_encoder
+    
+  end
+
+  def test_it_has_an_encrypter
+
+  end
+
+  def test_it_has_a_decrypter
+
+  end
+
+  def test_it_encrypts_a_message_no_key_or_date_given
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+
+    assert_equal expected, @enigma.encrypt("hello world")
+  end
+
+  def test_it_encrypts_a_message_key_given
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+
+    assert_equal expected, @enigma.encrypt("hello world", "02715")
+  end
 end
