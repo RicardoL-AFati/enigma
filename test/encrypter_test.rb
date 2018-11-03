bb5b544a22a911c76c228bb5974e7d93068acc88
Ricardo Ledesma <rickydem24@gmail.com>
2018-11-03 10:38:11 -0600

    changes expected to be substring rather than array

--- test/encrypter_test.rb
+++ test/encrypter_test.rb
@@ -1,11 +1,12 @@
-require 'test/test_helper'
+require './test/test_helper'
 
-class EncrypterTest < Mintest::Test
+class EncrypterTest < Minitest::Test
   def setup
     @encrypter = Encrypter.new
   end
 
   def test_it_encrypts_message
+    skip
     expected = "keder ohulw"
     result = @encrypter.encrypt("hello world",
       {A: 3, B: 27, C: 73, D: 20})
@@ -14,7 +15,7 @@ class EncrypterTest < Mintest::Test
   end
 
   def test_it_shifts_an_array_of_character_forwards
-    expected = ["k", "e", "d", "e"]
+    expected = "kede"
     result = @encrypter.shift(["h", "e", "l", "l"],
       [3, 27, 73, 20])
 
