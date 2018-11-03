f76cbd07369f0a04081996927757a05f1f8699e7
Ricardo Ledesma <rickydem24@gmail.com>
2018-11-03 12:50:50 -0600

    moves de(en)crypt to parent - now called scramble

--- lib/scrambler.rb
+++ lib/scrambler.rb
@@ -2,7 +2,17 @@ class Scrambler
   ABC_ = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
           "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
 
-  def shift(characters, shifts, backwards = false)
+  def scramble(message, shifts, backwards)
+    message_chars = message.downcase.chars
+    scrambled = ""
+
+    until message_chars.empty?
+      decrypted += shift(message_chars.slice!(0, 4), shifts, backwards)
+    end
+    scrambled
+  end
+
+  def shift(characters, shifts, backwards)
     characters.each_with_index.map do |char, index|
       next char unless char =~ /[a-z ]/
       original_index = ABC_.index(char)
