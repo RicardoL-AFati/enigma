b56a88a094d3749f28807164a484842fcfbd88a1
Ricardo Ledesma <rickydem24@gmail.com>
2018-11-04 15:10:28 -0700

    Adds refactor to runner files

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -2,16 +2,10 @@ require './lib/enigma'
 
 enigma = Enigma.new
 
-input_file = File.open(ARGV[0], "r")
-message = input_file.read
-input_file.close
+message = File.open(ARGV[0], "r").read
 
 decrypted = enigma.decrypt(message, ARGV[2], ARGV[3])
 
-output_file = File.open(ARGV[1], "w")
-output_file.write(encrypted[:encryption])
-output_file.close
+File.open(ARGV[1], "w").write(decrypted[:decryption])
 
-puts "Created #{ARGV[1]} with the key #{encrypted[:key]} and date #{encrypted[:date]}"
-
-require "pry"; binding.pry
+puts "Created #{ARGV[1]} with the key #{decrypted[:key]} and date #{decrypted[:date]}"
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -2,14 +2,10 @@ require './lib/enigma'
 
 enigma = Enigma.new
 
-input_file = File.open(ARGV[0], "r")
-message = input_file.read
-input_file.close
+message = File.open(ARGV[0], "r").read
 
 encrypted = enigma.encrypt(message)
 
-output_file = File.open(ARGV[1], "w")
-output_file.write(encrypted[:encryption])
-output_file.close
+File.open(ARGV[1], "w").write(encrypted[:encryption])
 
 puts "Created #{ARGV[1]} with the key #{encrypted[:key]} and date #{encrypted[:date]}"
