1295ef342c9aa3218bd208e6b3d1aa2f3f391582
Ricardo Ledesma <rickydem24@gmail.com>
2018-11-04 13:23:45 -0700

    Adds tests for decrypting messages - given both and given only key

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -99,13 +99,27 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, @enigma_2.encrypt("hello world", "02715", "040895")
   end
 
-  # def test_it_decrypts_a_message
-  #   expected = {
-  #     encryption: "keder ohulw",
-  #     key: "02715",
-  #     date: "040895"
-  #   }
-  #
-  #   assert_equal expected, @enigma_2.encrypt("hello world", "02715", "040895")
-  # end
+  def test_it_decrypts_a_message_given_key_and_date
+    skip
+    expected = {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+
+    assert_equal expected, @enigma_2.decrypt("keder ohulw", "02715", "040895")
+  end
+
+  def test_it_decrypts_a_message_given_key
+    skip
+    Time.stubs(:now).returns(Time.new(1995, 8, 4))
+
+    expected = {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+
+    assert_equal expected, @enigma_2.decrypt("keder ohulw", "02715")
+  end
 end
