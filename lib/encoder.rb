861f96c95983b376050547e32045a2465afc5d12
Ricardo Ledesma <rickydem24@gmail.com>
2018-11-05 12:16:27 -0700

    Moves generating final shift methods to enigma

--- lib/encoder.rb
+++ lib/encoder.rb
@@ -1,18 +1,4 @@
 class Encoder
-  def generate_shifts(key = nil, date = Time.now)
-    key_shifts = generate_key_shifts(key)
-    offset_shifts = generate_offset_shifts(date)
-
-    form_final_shifts(key_shifts, offset_shifts)
-  end
-
-  def form_final_shifts(key_shifts, offset_shifts)
-    key_shifts.keys.reduce({}) do |shifts, letter|
-      shifts[letter] = key_shifts[letter] + offset_shifts[letter]
-      shifts
-    end
-  end
-
   def empty_shifts
     {A: "", B: "", C: "", D: ""}
   end
