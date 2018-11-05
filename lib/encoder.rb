c44aa777df1f9e7bde1b0e2fccf3d6e4d24f040e
Ricardo Ledesma <rickydem24@gmail.com>
2018-11-05 11:25:08 -0700

    Moves date and offset methods from encoder to new subclass

--- lib/encoder.rb
+++ lib/encoder.rb
@@ -6,31 +6,11 @@ class Encoder
     form_final_shifts(key_shifts, offset_shifts)
   end
 
-  def generate_offset_shifts(date)
-    form_offset_shifts(get_offset_code(date))
-  end
-
-  def get_offset_code(date)
-    date = in_DD_MM_YY(date) unless date.class == String
-    (date.to_i ** 2).to_s[-4..-1].chars
-  end
-
   def form_final_shifts(key_shifts, offset_shifts)
     key_shifts.keys.reduce({}) do |shifts, letter|
       shifts[letter] = key_shifts[letter] + offset_shifts[letter]
       shifts
     end
-  end
-
-  def form_offset_shifts(offset_code)
-    offset_code.each_with_index.reduce(empty_shifts) do |shifts, (num, index)|
-      shifts[shifts.keys[index]] = num.to_i
-      shifts
-    end
-  end
-
-  def in_DD_MM_YY(date)
-    date.strftime("%d%m%y")
   end
 
   def empty_shifts
